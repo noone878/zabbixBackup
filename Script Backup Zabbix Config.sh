f604e60e5c7e4256e4328996ecc8053ef396b77c
Septian Al Rizki <122249494+noone878@users.noreply.github.com>
2024-03-09 23:25:46 +0700

    zabbixBackupScript
    Backup Direktories :
    - /etc/zabbix
    - /etc/apache2
    - /usr/share/zabbix
    - /usr/share/doc/zabbix-web-service" "/usr/share/doc/zabbix-sql-scripts" "/usr/share/doc/zabbix-server-mysql" "/usr/share/doc/zabbix-release" "/usr/share/doc/zabbix-frontend-php" "/usr/share/doc/zabbix-apache-conf" "/usr/share/doc/zabbix-agent2
    
    To create a destination folder you need to create a zabbix-backup folder in the /opt directory :
    Example :
    - /opt/zabbix-backup/zabbix-config
    - /opt/zabbix-backup/web-config
    - /opt/zabbix-backup/share/zabbix
    - /opt/zabbix-backup/share/doc
    
    Backup results will be deleted after 7days.

--- Script Backup Zabbix Config.sh
+++ Script Backup Zabbix Config.sh
@@ -1,73 +1,70 @@
+
+# Skrip ini ditulis oleh: Septian Al Rizki
+# Skrip ini tidak boleh dimodifikasi
+
 #!/bin/bash
 
-# Zabbix direktori 
+# Source Zabbix direktori 
 backup_directories1=("/etc/zabbix")
 
-# Web direktori
+# Source Web direktori
 backup_directories2=("/etc/apache2")
 
-# Shared direktori
+# Source Shared direktori
 backup_directories3=("/usr/share/zabbix")
 
 # Doc direktori 
-backup_directories4=("/usr/share/doc/zabbix-web-service" "/usr/share/doc/zabbix-sql-scripts" "/usr/share/doc/zabbix-server-mysql" "/usr/share/doc/zabbix-release" "/usr/share/doc/zabbix-frontend-php" "/usr/share/doc/apache-conf" "/usr/share/doc/zabbix-agent2")
+backup_directories4=("/usr/share/doc/zabbix-web-service" "/usr/share/doc/zabbix-sql-scripts" "/usr/share/doc/zabbix-server-mysql" "/usr/share/doc/zabbix-release" "/usr/share/doc/zabbix-frontend-php" "/usr/share/doc/zabbix-apache-conf" "/usr/share/doc/zabbix-agent2")
 
-# Daftar lokasi destinasi Zabbix
+# Destinations Backup Zabbix
 backup_locations1=("/opt/zabbix-backup/zabbix-config")
 
-# Daftar lokasi destinasi Web
-backup_locations2=("/opt/zabbix-backup/web-config/apache2")
+# Destinations Backup  Web
+backup_locations2=("/opt/zabbix-backup/web-config")
 
-# Daftar lokasi destinasi Shared
-backup_locations3=("/opt/zabbix-backup/shared/zabbix")
+# Destinations Backup  Shared
+backup_locations3=("/opt/zabbix-backup/share/zabbix")
 
-# Daftar lokasi destinasi Doc
-backup_locations4=("/opt/zabbix-backup/shared/doc")
+# Destinations Backup  Doc
+backup_locations4="/opt/zabbix-backup/share/doc"
+Zabbix_Docs="zabbix_docs_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
-# Informasi koneksi MySQL
-db_user="root"
-db_password="varni0nz4bb1x"
-db_name="zabbix"
-db_host="172.17.4.40"
 
-# Informasi koneksi SSH
-ssh_user="zabbix"
-ssh_password="varnionzabbix"
+# Information MySQL
+local_user="zabbix"
+local_password="varni0nz4bb1x"
+database_name="zabbix"
 
-# Lokasi destinasi untuk backup
-backup_location=("/opt/zabbix-backup/database")
 
-# Nama yang mirip yang akan dicocokkan
+# Destinations Backup DB
+backup_locations=("/opt/zabbix-backup/database")
+
+# Similar Name DB
 similar_name="zabbix"
 
-# Buat nama file backup untuk database dengan timestamp
-backup_file="$backup_location/zabbix_mysql_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
+# Create name backup with timestamp
+backup_file="$backup_locations/zabbix_mysql_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
 
-# Kombinasikan variabel-variabel untuk perintah SSH
-SSH_CMD="sshpass -p '$ssh_password' ssh -o StrictHostKeyChecking=no $ssh_user@$db_host"
 
-# Lakukan backup MySQL database dari server remote menggunakan mysqldump melalui SSH
-$SSH_CMD "mysqldump -u $db_user -p $db_password -h $db_host $db_name" > "$backup_file"
+# Back up the MySQL database from a remote server using mysqldump
+mysqldump -h localhost -u "$local_user" -p"$local_password" "$database_name" > "$backup_file"
 
-# ssh zabbix@${db_host} "mysqldump -u $db_user -p $db_password -h $db_host $db_name" > "$backup_file"
-
-# Tampilkan pesan berdasarkan keberhasilan backup
+# Show messages based on backup success
 if [ $? -eq 0 ]; then
-    echo "Backup database $db_name dari host $db_host berhasil disimpan di: $backup_file"
+    echo "Backup database $database_name dari host berhasil disimpan di: $backup_file"
 else
-    echo "Backup database $db_name dari host $db_host gagal"
+    echo "Backup database $database_name dari host gagal"
 fi
-
 
 # Zabbix Config
 for ((i=0; i<${#backup_directories1[@]}; i++)); do
-    # Buat nama file backup dengan timestamp
-    zabbix_config="${backup_locations1[$i]}/Zabbix_Config_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
+    # Create backup file name with timestamp
+    zabbix_config="${backup_locations1[$i]}/zabbix_config_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
-    # Lakukan backup menggunakan tar
+    # Do a backup using tar
     tar -czf "$zabbix_config" "${backup_directories1[$i]}"
 
-    # Tampilkan pesan berdasarkan keberhasilan backup
+    # show message based on backup success
     if [ $? -eq 0 ]; then
         echo "Backup untuk ${backup_directories1[$i]} berhasil disimpan di: $zabbix_config"
     else
@@ -75,15 +72,16 @@ for ((i=0; i<${#backup_directories1[@]}; i++)); do
     fi
 done
 
+
 # Zabbix Web
 for ((i=0; i<${#backup_directories2[@]}; i++)); do
-    # Buat nama file backup dengan timestamp
-    zabbix_apache2="${backup_locations2[$i]}/Zabbix_Apache2_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
+    # Create backup file name with timestamp
+    zabbix_apache2="${backup_locations2[$i]}/zabbix_apache2_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
-    # Lakukan backup menggunakan tar
+    # Do a backup using tar
     tar -czf "$zabbix_apache2" "${backup_directories2[$i]}"
 
-    # Tampilkan pesan berdasarkan keberhasilan backup
+    # show message based on backup success
     if [ $? -eq 0 ]; then
         echo "Backup untuk ${backup_directories2[$i]} berhasil disimpan di: $zabbix_apache2"
     else
@@ -91,15 +89,16 @@ for ((i=0; i<${#backup_directories2[@]}; i++)); do
     fi
 done
 
+
 # Zabbix Shared
 for ((i=0; i<${#backup_directories3[@]}; i++)); do
-    # Buat nama file backup dengan timestamp
-    zabbix_shared="${backup_locations3[$i]}/Zabbix_Shared_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
+    # Create backup file name with timestamp
+    zabbix_shared="${backup_locations3[$i]}/zabbix_shared_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
-    # Lakukan backup menggunakan tar
+    # Do a backup using tar
     tar -czf "$zabbix_shared" "${backup_directories3[$i]}"
 
-    # Tampilkan pesan berdasarkan keberhasilan backup
+    # show message based on backup success
     if [ $? -eq 0 ]; then
         echo "Backup untuk ${backup_directories3[$i]} berhasil disimpan di: $zabbix_shared"
     else
@@ -108,17 +107,15 @@ for ((i=0; i<${#backup_directories3[@]}; i++)); do
 done
 
 # Zabbix Docs
+# Create backup file name with timestamp
 for ((i=0; i<${#backup_directories4[@]}; i++)); do
-    # Buat nama file backup dengan timestamp
-    zabbix_doc="${backup_locations4[$i]}/Zabbix_Docs_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
-
-    # Lakukan backup menggunakan tar
-    tar -czf "$zabbix_doc" "${backup_directories4[$i]}"
-
-    # Tampilkan pesan berdasarkan keberhasilan backup
-    if [ $? -eq 0 ]; then
-        echo "Backup untuk ${backup_directories4[$i]} berhasil disimpan di: $zabbix_doc"
-    else
-        echo "Backup untuk ${backup_directories4[$i]} gagal"
-    fi
+  # Add files to archive
+  tar -rf "$backup_locations4/$Zabbix_Docs" "${backup_directories4[$i]}"
 done
+
+# show message based on backup succes
+echo "Backup Docs direktori berhasil disimpan di: $backup_locations4/$Zabbix_Docs"
+
+# Find and delete backup files that are more than 7 days old, except zabbix_backup.sh and directories
+find /opt/zabbix-backup/* -type f -mtime +7 ! -name "zabbix_backup.sh" -exec rm {} \;
+
