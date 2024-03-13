354b9ea13b36e719f6b1f5415e26485866cb90b6
Septian Al Rizki <122249494+noone878@users.noreply.github.com>
2024-03-13 15:15:26 +0700

    zabbix_restore.sh

--- zabbix_restore.sh
+++ zabbix_restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Informasi koneksi MySQL
+# Information Connection MySQL
 db_user="mysql_user"
 db_password="mysql_password"
 db_name="mysql_database"
 
-# Direktori tempat backup database dan folder disimpan
+# Backup Directory
 # DB
 backup_directoryDB="/opt/zabbix-backup/database"
 # Zabbix
@@ -17,10 +17,10 @@ backup_directory3="/opt/zabbix-backup/share/zabbix"
 # Docs
 backup_directory4="/opt/zabbix-backup/share/doc"
 
-# Mencari file backup database terbaru
+# Find New Latest Backup DB
 latest_database_backupDB=$(ls -t "$backup_directoryDB"zabbix_mysql_* | head -n 1)
 
-# Mencari file backup folder terbaru
+# Find New Latest Backup Folder
 # Zabbix
 latest_folder_backup1=$(ls -t "$backup_directory1"/zabbix_config_*.tar.gz | head -n 1)
 # Web
@@ -31,29 +31,29 @@ latest_folder_backup3=$(ls -t "$backup_directory3"/zabbix_shared_*.tar.gz | head
 latest_folder_backup4=$(ls -t "$backup_directory4"/zabbix_docs_*.tar.gz | head -n 1)
 
 
-# Check DB Found it?
+# Check DB / folder Found it?
 if [ -z "$latest_database_backupDB" ] || [ -z "$latest_folder_backup1" ] || [ -z "$latest_folder_backup2" ] || [ -z "$latest_folder_backup3" ] || [ -z "$latest_folder_backup4" ]; then
-    echo "Tidak dapat menemukan file backup database atau folder terbaru."
+    echo "Cannot find the latest database backup file or folder."
     exit 1
 fi
 
-# Perintah untuk restore database
+# restore database
 mysql -u "$db_user" -p"$db_password" "$db_name" < "$latest_database_backupDB"
 
-# Memeriksa keberhasilan restore dse
+# Check the success of the restore
 restore_status=$?
 
-# Tampilkan pesan berdasarkan keberhasilan restore database
+# Display a message based on the success of the database restore
 if [ $restore_status -eq 0 ]; then
-    echo "Restore database $db_name dari $latest_database_backupDB berhasil"
+    echo "Restore database $db_name dari $latest_database_backupDB success"
 else
-    echo "Restore database $db_name dari $latest_database_backupDB gagal"
-    echo "Detail kesalahan:"
-    mysql -u "$db_user" -p"$db_password" "$db_name" < "$latest_database_backupDB" 2>&1 | sed 's/^/  /'  # Menampilkan detail kesalahan dengan indentasi
+    echo "Restore database $db_name dari $latest_database_backupDB fail"
+    echo "Information Error:"
+    mysql -u "$db_user" -p"$db_password" "$db_name" < "$latest_database_backupDB" 2>&1 | sed 's/^/  /'  # Displays error details with indentation
     exit $restore_status
 fi
 
-# Perintah untuk restore folder
+# restore folder
 # Zabbix
 restore_destination1="/etc/"
 tar -xzf "$latest_folder_backup1" -C "$restore_destination1"
@@ -68,34 +68,34 @@ restore_destination4=("/usr/share/doc/" "/usr/share/doc/" "/usr/share/doc/" "/us
 
 tar -xvpf "$latest_folder_backup4" -C "$restore_destination4"
 
-# Tampilkan pesan berdasarkan keberhasilan restore folder
+# Show a message based on successful folder restore
 if [ $? -eq 0 ]; then
-    echo "Restore folder dari $latest_folder_backup1 berhasil di-$restore_destination1"
+    echo "Restore folder from $latest_folder_backup1 success in-$restore_destination1"
 else
-    echo "Restore folder dari $latest_folder_backup1 gagal"
+    echo "Restore folder from $latest_folder_backup1 fail"
 fi
 if [ $? -eq 0 ]; then
-    echo "Restore folder dari $latest_folder_backup2 berhasil di-$restore_destination2"
+    echo "Restore folder from $latest_folder_backup2 success in-$restore_destination2"
 else
-    echo "Restore folder dari $latest_folder_backup2 gagal"
+    echo "Restore folder from $latest_folder_backup2 fail"
 fi
 if [ $? -eq 0 ]; then
-    echo "Restore folder dari $latest_folder_backup3 berhasil di-$restore_destination3"
+    echo "Restore folder from $latest_folder_backup3 success in-$restore_destination3"
 else
-    echo "Restore folder dari $latest_folder_backup3 gagal"
+    echo "Restore folder from $latest_folder_backup3 fail"
 fi
 
 # Docs
 for latest_folder_backup4 in "${latest_folder_backups4[@]}"; do
     for restore_destination4 in "${restore_destinations4[@]}"; do
-        # Perintah untuk restore folder
+        # restore folder
         tar -xzf "$latest_folder_backup4" -C "$restore_destination4"
 
-        # Tampilkan pesan berdasarkan keberhasilan restore folder
+        # Show a message based on successful folder restore
         if [ $? -eq 0 ]; then
-            echo "Restore folder dari $latest_folder_backup4 berhasil di-$restore_destination4"
+            echo "Restore folder from $latest_folder_backup4 success in-$restore_destination4"
         else
-            echo "Restore folder dari $latest_folder_backup4 gagal di-$restore_destination4"
+            echo "Restore folder from $latest_folder_backup4 fail in-$restore_destination4"
         fi
     done
 done
